3e365cdde967d15983b8cc449c0c5c9180a708c8
Edoardo Cunsolo <127218728+SharkEdo@users.noreply.github.com>
2023-05-29 12:59:06 +0200

    Update homepage.js
    Aggiunta colore bordo dopo click del like

--- homepage.js
+++ homepage.js
@@ -163,6 +163,8 @@ ButtH.addEventListener("submit", searchH);
 
 function saveSong(event){
   // Preparo i dati da mandare al server e invio la richiesta con POST
+  likecol = event.currentTarget;
+  likecol.style.border = "5px solid green";
   console.log("Salvataggio")
   // get parent card
   const card = event.currentTarget.parentNode;
